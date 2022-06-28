71cb64a71ac5504214c7477e9adc4572aca46c4d
Mohamed <mf1456@fayoum.edu.eg>
2022-06-28 15:25:30 +0200

    services section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ let menuContent = document.querySelector('.menu-content');
 menuToggle.onclick = () => {
   menuContent.classList.toggle('d-none');
 }
+
+let forGround = document.querySelector(".forGround")
+let backGround = document.querySelector(".backGround")
+forGround.onmouseover = () => {
+  backGround.classList.add("forGroundHover")
+}
+forGround.onmouseout = () => {
+  backGround.classList.remove("forGroundHover")
+}
